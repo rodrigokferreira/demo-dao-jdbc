c5f6a17c71e6f161f8de959934d529d5f8d9f1e7
Rodrigo Kikuchi <89669642+rodrigokferreira@users.noreply.github.com>
2024-10-01 16:19:17 -0300

    implementation FindAll

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -15,16 +17,16 @@ public class Program2 {
 		DepartmentDao depDao = DaoFactory.createDepartmentDao();
 		Department dep = new Department();
 		
-		/* System.out.println("\n=== TEST 1: Department insert =======");
+		System.out.println("\n=== TEST 1: Department insert =======");
 		Department newDep = new Department(null, "Manager");
 		depDao.insert(newDep);
-		System.out.print("Inserted! " + newDep.getId()); */
+		System.out.print("Inserted! " + newDep.getId()); 
 		
-		/* System.out.println("\n=== TEST 2: Department Update =======");
+		 System.out.println("\n=== TEST 2: Department Update =======");
 		dep = depDao.findById(6);
 		dep.setName("Maintenance");
 		depDao.update(dep);
-		System.out.println("Update completed!");  */
+		System.out.println("Update completed!");  
 		
 		System.out.println("\n=== TEST 3: Department findById =======");
 		dep = depDao.findById(3);
@@ -35,6 +37,15 @@ public class Program2 {
 		int op = sc.nextInt();
 		depDao.deleteById(op);
 		System.out.println("Deleted!");
+		
+		System.out.println("\n=== TEST 5: Department findAll =======");
+		List<Department> list = new ArrayList<>();
+		list = depDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		sc.close();
 		
 	}
 
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,13 +5,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
 
 import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class DepartmentDaoJDBC implements DepartmentDao{
 
@@ -124,14 +125,33 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	    PreparedStatement st = null;
+	    ResultSet rs = null;
+	    try {
+	        st = conn.prepareStatement("SELECT * FROM department");
+	        rs = st.executeQuery();
+	        
+	        List<Department> list = new ArrayList<>();
+	        
+	        while (rs.next()) {
+	            Department dep = instantiateDepartment(rs);
+	            list.add(dep);
+	        }
+	        return list;
+	    } 
+	    catch (SQLException e) {
+	        throw new DbException(e.getMessage());
+	    } 
+	    finally {
+	        DB.closeStatement(st);
+	        DB.closeResultSet(rs);
+	    }
 	}
 	
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
 	    Department dep = new Department();
-	    dep.setId(rs.getInt("Id"));  // Corrigido para "Id"
-	    dep.setName(rs.getString("Name"));  // Corrigido para "Name"
+	    dep.setId(rs.getInt("Id"));  // 
+	    dep.setName(rs.getString("Name"));  // 
 	    return dep;
 	}
 	
