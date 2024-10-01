9af65b63aa7dbb683c2eb8c39cc6b0e4d4b07684
Rodrigo Kikuchi <89669642+rodrigokferreira@users.noreply.github.com>
2024-10-01 02:12:43 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,9 +31,15 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== TEST 4: seller insert =======");
-		Seller newSeller = new Seller(null, "Gred", "greg@gmail.com", new Date(), 4000.00, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New Id: = " + newSeller.getId());
+		//System.out.println("\n=== TEST 4: seller insert =======");
+		//Seller newSeller = new Seller(null, "Gred", "greg@gmail.com", new Date(), 4000.00, department);
+		//sellerDao.insert(newSeller);
+		//System.out.println("Inserted! New Id: = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update =======");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
